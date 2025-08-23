b670933ff40f0f34e3fc9fa5b5992b0079d99f85
Muhammad Hassan <sh.hassan607@gmail.com>
2025-08-24 00:27:31 +0500

    Time: 0 ms (100%), Space: 9.8 MB (76.05%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>maxHeap;
+
+        for(auto stone:stones){
+            maxHeap.push(stone);
+        }
+
+        while(maxHeap.size()>1){
+            int y = maxHeap.top();
+            maxHeap.pop();
+            int x = maxHeap.top();
+            maxHeap.pop();
+            if( x==y){
+                maxHeap.push(0);
+            }
+            if(x != y){
+                maxHeap.push(y-x);
+            }
+        }
+        return maxHeap.size() > 0 ? maxHeap.top() : 0;
+    }
+};
