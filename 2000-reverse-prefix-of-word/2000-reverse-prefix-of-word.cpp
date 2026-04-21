ee115dede2826b5c313f8ae4545c94078ded59c2
Muhammad Hassan <sh.hassan607@gmail.com>
2026-04-21 19:12:57 +0500

    Time: 0 ms (100%), Space: 8.3 MB (65.74%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int left = 0, right = 0;
+        for (int i = 0; i < word.size(); i++) {
+            if (word[i] == ch) {
+                right = i;
+                break;
+            }
+        }
+        while (left <= right) {
+            swap(word[left], word[right]);
+            left++;
+            right--;
+        }
+        return word;
+    }
+};
