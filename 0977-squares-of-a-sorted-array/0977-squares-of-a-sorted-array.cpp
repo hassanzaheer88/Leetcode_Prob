99ef7e84e40695724e56822b8011591756c5b96e
Muhammad Hassan <sh.hassan607@gmail.com>
2026-08-19 09:21:50 +0500

    Time: 555 ms (6.1%), Space: 30.2 MB (66.5%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -4,7 +4,13 @@ public:
         for(int i=0;i<nums.size();i++){
             nums[i] = nums[i] * nums[i];
         }
-        sort(nums.begin(),nums.end());
+        for (int i =0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]>nums[j]){
+                    swap(nums[i],nums[j]);
+                }
+            }
+        }
         return nums;
     }
 };
